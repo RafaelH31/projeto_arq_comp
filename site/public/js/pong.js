3e76fd52adb5b303f91e816ca243d0dbe183c0f1
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-06-20 20:43:16 -0300

    pong

--- site/public/js/pong.js
+++ site/public/js/pong.js
@@ -10,73 +10,61 @@ var qtdclick = 0
 
     canvas.width = 600;
     canvas.height = 400;
-
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     ctx.fillStyle = 'green';
 
-    var leftRaqtY = 175;
-    var rightRaqtY = 175;
 
-    var leftRaqtScore = 0;
-    var rightRaqtScore = 0;
 
+    var leftraqtY = 175;
+    var rightraqtY = 175;
+    var leftraqtpont = 0;
+    var rightraqtpont = 0;
     var bolaX = 300;
     var bolaY = 200;
     var bolavelX = -5;
     var bolavelY = -5;
-
-    var gamon = false;
-
+    var gameon = false;
     var gameinter;
+
+
 
     function draw() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-
       ctx.fillStyle = 'black';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
-
       ctx.fillStyle = 'green';
-      ctx.fillRect(50, leftRaqtY, 25, 50);
-      ctx.fillRect(525, rightRaqtY, 25, 50);
-
+      ctx.fillRect(50, leftraqtY, 25, 50);
+      ctx.fillRect(525, rightraqtY, 25, 50);
       ctx.fillStyle = 'white';
       ctx.font = '24px Arial';
-      ctx.fillText('Score: ' + leftRaqtScore, 50, 30);
-      ctx.fillText('Score: ' + rightRaqtScore, canvas.width - 150, 30);
-
+      ctx.fillText('pontos: ' + leftraqtpont, 50, 30);
+      ctx.fillText('pontos: ' + rightraqtpont, canvas.width - 150, 30);
       drawbola();
     }
-
     function drawbola() {
-      // Update the position of the bola
       bolaX += bolavelX;
       bolaY += bolavelY;
-
       if (bolaY < 0 || bolaY > canvas.height) {
         bolavelY = -bolavelY;
       }
-
       if (
-        (bolaX < 75 && bolaY > leftRaqtY && bolaY < leftRaqtY + 50) ||
-        (bolaX > 525 && bolaY > rightRaqtY && bolaY < rightRaqtY + 50)
+        (bolaX < 75 && bolaY > leftraqtY && bolaY < leftraqtY + 50) ||
+        (bolaX > 525 && bolaY > rightraqtY && bolaY < rightraqtY + 50)
       ) {
         bolavelX = -bolavelX;
       }
 
       if (bolaX < 0) {
-        rightRaqtScore++;
-
-        if (rightRaqtScore === 10) {
+        rightraqtpont++;
+        if (rightraqtpont === 10) {
           endgame('Right');
         } else {
           resetbola();
         }
       } else if (bolaX > canvas.width) {
-        leftRaqtScore++;
-
-        if (leftRaqtScore === 10) {
+        leftraqtpont++;
+        if (leftraqtpont === 10) {
           endgame('Left');
         } else {
           resetbola();
@@ -89,6 +77,7 @@ var qtdclick = 0
       ctx.fill();
     }
 
+
     function resetbola() {
       bolaX = 300;
       bolaY = 200;
@@ -96,53 +85,56 @@ var qtdclick = 0
       bolavelY = -5;
     }
 
-    function endgamee(winner) {
+    function endgame(winner) {
   bolavelX = 0;
   bolavelY = 0;
 
   if (winner == 'Right') {
+    //o post aqui
     ctx.fillStyle = 'white';
     ctx.font = '48px Arial';
     ctx.fillText(winner + ' Player Wins!', canvas.width / 2 - 150, canvas.height / 2);
   }
   if(winner =='Left'){
+    //ou aqui
     ctx.fillStyle = 'white';
     ctx.font = '48px Arial';
     ctx.fillText(winner + ' Player Wins!', canvas.width / 2 - 150, canvas.height / 2);
   }
 
   clearInterval(gameinter);
-  gamon = false; 
+  gameon = false; 
 }
 
 
-    function startGame() {
-  if (!gamon) {
-    leftRaqtScore = 0;
-    rightRaqtScore = 0;
+
+    function startgame() {
+  if (!gameon) {
+    leftraqtpont = 0;
+    rightraqtpont = 0;
     resetbola();
 
-    gamon = true;
+    gameon = true;
     gameinter = setInterval(draw, 1000 / 60);
   } 
   if(qtdclick > 0){
-//o post aqui
+
 
   }
   qtdclick ++
 }
 
     document.addEventListener('keydown', function(event) {
-      if (event.key === 'w' && leftRaqtY > 0) {
-        leftRaqtY -= 25;
+      if (event.key === 'w' && leftraqtY > 0) {
+        leftraqtY -= 25;
       }
-      else if (event.key === 's' && leftRaqtY < canvas.height - 50) {
-        leftRaqtY += 25;
+      else if (event.key === 's' && leftraqtY < canvas.height - 50) {
+        leftraqtY += 25;
       }
-      else if (event.key === 'ArrowUp' && rightRaqtY > 0) {
-        rightRaqtY -= 25;
+      else if (event.key === 'ArrowUp' && rightraqtY > 0) {
+        rightraqtY -= 25;
       }
-      else if (event.key === 'ArrowDown' && rightRaqtY < canvas.height - 50) {
-        rightRaqtY += 25;
+      else if (event.key === 'ArrowDown' && rightraqtY < canvas.height - 50) {
+        rightraqtY += 25;
       }
     });
